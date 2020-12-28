52dbfb1227ec2d870ca3589183dcf16626b7f53f
SrutiPraturi <39187327+SrutiPraturi@users.noreply.github.com>
2020-12-28 14:02:49 +0530

    Update git_shell.sh

--- git_shell.sh
+++ git_shell.sh
@@ -6,7 +6,7 @@ git init
 git config --global user.email "sruti_praturi@gmail.com"
 git config --global user.name "Sruti "
 git remote rm origin 
-git remote add origin https://gitlab.com/SrutiPraturi/motor_insurance_serving.git
+git remote add origin https://SrutiPraturi:Git7889@github.com/SrutiPraturi/motor_insurance_serving.git
 git pull origin master
 git add assets
 git commit -m "assets commit"
