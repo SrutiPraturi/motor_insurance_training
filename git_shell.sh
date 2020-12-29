e091148299073d9ef788cfb4550810ae97cd0a07
SrutiPraturi <39187327+SrutiPraturi@users.noreply.github.com>
2020-12-29 15:43:32 +0530

    Update git_shell.sh

--- git_shell.sh
+++ git_shell.sh
@@ -1,8 +1,8 @@
 mkdir gitmove
-set DATESTR=%date:~10,4%-%date:~-7,2%-%date:~4,2%-%time:~0,2%-%time:~3,2%-%time:~6,2%
-mkdir gitmove/%DATESTR% 
+dir="$(date +%s)"
+mkdir gitmove/$(dir) 
 
-cp -r assets/* gitmove/%DATESTR%/
+cp -r assets/* gitmove/$(dir)/
 cd gitmove
 git init
 git config --global user.email "sruti_praturi@gmail.com"
@@ -10,6 +10,6 @@ git config --global user.name "Sruti "
 git remote rm origin 
 git remote add origin https://SrutiPraturi:Git7889@github.com/SrutiPraturi/motor_insurance_serving.git
 git pull origin master
-git add %DATESTR%
+git add $(dir)
 git commit -m "assets commit"
 git push --set-upstream origin master   
