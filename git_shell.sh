e5265e7a13f959d8fb124759f989d0f723a4d077
SrutiPraturi <39187327+SrutiPraturi@users.noreply.github.com>
2020-12-29 15:52:21 +0530

    Update git_shell.sh

--- git_shell.sh
+++ git_shell.sh
@@ -1,8 +1,8 @@
 mkdir gitmove
 dir="$(date +%s)"
-mkdir gitmove/$(dir) 
+mkdir gitmove/${dir} 
 
-cp -r assets/* gitmove/$(dir)/
+cp -r assets/* gitmove/${dir}/
 cd gitmove
 git init
 git config --global user.email "sruti_praturi@gmail.com"
@@ -10,6 +10,6 @@ git config --global user.name "Sruti "
 git remote rm origin 
 git remote add origin https://SrutiPraturi:Git7889@github.com/SrutiPraturi/motor_insurance_serving.git
 git pull origin master
-git add $(dir)
+git add ${dir}
 git commit -m "assets commit"
 git push --set-upstream origin master   
