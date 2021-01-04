1274ff87550f320996040f41c87f770e2cc42bf6
SrutiPraturi <39187327+SrutiPraturi@users.noreply.github.com>
2021-01-04 09:59:02 +0530

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -7,46 +7,49 @@ import yaml
 import numpy as np
 import pickle 
 
-def load_data(data):
-    data = pd.read_csv(data)
-    
-    return data 
+class ModelTrain:
 
-def train_model(data):
-    x=data.drop(['Payment'],axis =1)
-    y=data['Payment']
+    def load_data(self,data):
+        data = pd.read_csv(data)
+    
+        return data 
+
+    def train_model(self,data):
+        x=data.drop(['Payment'],axis =1)
+        y=data['Payment']
 
 
-    x_train , x_test , y_train , y_test = train_test_split(x,y, test_size = 0.2 , random_state = 42)
+        x_train , x_test , y_train , y_test = train_test_split(x,y, test_size = 0.2 , random_state = 42)
     
-    x_train.to_csv('datasets/insurance_model_features.csv',index = False)
+        x_train.to_csv('datasets/insurance_model_features.csv',index = False)
     
     
 
-    model = LinearRegression().fit(x_train , y_train)
-    training_RMSE = np.sqrt(mse(np.expm1(y_train) , np.expm1(model.predict(x_train))))
-    test_RMSE = np.sqrt(mse(np.expm1(y_test), np.expm1(model.predict(x_test))))
+        model = LinearRegression().fit(x_train , y_train)
+        training_RMSE = np.sqrt(mse(np.expm1(y_train) , np.expm1(model.predict(x_train))))
+        test_RMSE = np.sqrt(mse(np.expm1(y_test), np.expm1(model.predict(x_test))))
     
-    model_r2 = r2_score(y_test,model.predict(x_test))
+        model_r2 = r2_score(y_test,model.predict(x_test))
     
-    features = x_train.columns
+        features = x_train.columns
     
-    final_data = x_train
-    final_data = final_data.assign(Payment = y_train)
+        final_data = x_train
+        final_data = final_data.assign(Payment = y_train)
     
-    final_data.to_csv('datasets/insurance_data.to_csv',index = False)
+        final_data.to_csv('datasets/insurance_data.to_csv',index = False)
     
     
     
-    d = {'features' : list(features) , 'training_RMSE': float(training_RMSE), 'test_RMSE': float(test_RMSE) , 'Model R2' : float(model_r2)}
+        d = {'features' : list(features) , 'training_RMSE': float(training_RMSE), 'test_RMSE': float(test_RMSE) , 'Model R2' : float(model_r2)}
     
-    file = open('assets/claims_model.pkl','wb')
-    pickle.dump(model , file)
-    file.close()
+        file = open('assets/claims_model.pkl','wb')
+        pickle.dump(model , file)
+        file.close()
     
-    with open('assets/baseline.yaml', 'w') as yaml_file:
-        yaml.dump(d, yaml_file, default_flow_style=False)
+        with open('assets/baseline.yaml', 'w') as yaml_file:
+            yaml.dump(d, yaml_file, default_flow_style=False)
         
 if __name__ == "__main__":
-    data= load_data('assets/insurance_processed_data.csv')
-    train_model(data)
+    modeltrain = ModelTrain()
+    data= modeltrain.load_data('assets/insurance_processed_data.csv')
+    modeltrain.train_model(data)
